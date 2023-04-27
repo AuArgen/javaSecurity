72389614d08e6df5f6e9c68ec15f7dc0764dfb35
AuArgen <argenazizbekuulu3@gmail.com>
2023-04-27 09:37:31 +0600

    gitignor updatte

--- src/main/java/com/example/demo/payload/request/SignUpRequest.java
+++ src/main/java/com/example/demo/payload/request/SignUpRequest.java
@@ -13,9 +13,14 @@ public class SignUpRequest {
     @NotBlank(message = "User email is required")
     @ValidEmail
     private String email;
+    @NotEmpty(message = "Please enter you name")
     private String firstname;
+    @NotEmpty(message = "Please enter you lastname")
     private String lastname;
+    @NotEmpty(message = "Please enter you usernmae")
     private String username;
+    @NotEmpty(message = "Password i required")
+    @Size(min = 6)
     private String password;
     private String confirmPassword;
 }
